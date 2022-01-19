b76f530b7829b8199275b3183e3c6afd20c17566
nikhilagnihotri27 <73015245+nikhilagnihotri27@users.noreply.github.com>
2022-01-19 19:41:39 +0530

    Smart banner added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {React} from 'react';
 import SignupAndLogin from './components/signupAndLogin';
 import TodoList from './components/todoList';
+import SmartBanner from 'react-smartbanner';
+import '../node_modules/react-smartbanner/dist/main.css';
 
 const App = () => {
 
   return (
     <div className="App">
+      <p>Smart Banner</p>
+      <SmartBanner title={'Facebook'} />
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
@@ -17,7 +21,7 @@ const App = () => {
             <TodoList />
           </Route>
         </Switch>
-      </BrowserRouter> 
+      </BrowserRouter>
     </div>
   );
 }
